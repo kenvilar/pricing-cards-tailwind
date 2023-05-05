f00342dffce83e938db012b0b26d448ed3ce0817
kenvilar <kenvilar@gmail.com>
2023-05-05 11:42:59 +0800

    fix column 1

--- src/app/Column1.tsx
+++ src/app/Column1.tsx
@@ -8,7 +8,7 @@ const Column1 = () => {
   return (
     <>
       <div className="bg-slate-700 rounded-xl text-white">
-        <div className="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+        <div className="p-9 mx-3 mt-3 rounded-t-xl bg-slate-800">
           <div className="text-center uppercase">Basic</div>
           <h2 className="mt-10 font-serif text-5xl text-center">100GB</h2>
           <h3 className="mt-2 text-center">$1.99/Month</h3>
@@ -20,34 +20,32 @@ const Column1 = () => {
               Purchase
             </a>
           </div>
+        </div>
 
-          <div className="border-t border-slate-700" />
+        <div className="border-t border-slate-700" />
 
-          <div className="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
-            <div className="flex flex-col space-y-2">
-              {items.map((o) => {
-                return (
-                  <>
-                    <div className="flex justify-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M5 12l5 5l10 -10" />
-                      </svg>
-                      <span className="text-sm ml-1">{o}</span>
-                    </div>
-                  </>
-                );
-              })}
-            </div>
+        <div className="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+          <div className="flex flex-col space-y-2">
+            {items.map((item: any) => {
+              return (
+                <div className="flex justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M5 12l5 5l10 -10" />
+                  </svg>
+                  <span className="text-sm ml-1">{item}</span>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
