8ac1426dd4ef6a172c6639b79ef5050552599f3f
kenvilar <kenvilar@gmail.com>
2023-05-05 11:35:50 +0800

    implement three cards for pricing

---
+++ src/app/Column3.tsx
@@ -0,0 +1,56 @@
+const Column3 = () => {
+  const items = [
+    "200 GB of storage",
+    "Option to add members",
+    "Extra member benefits",
+  ];
+
+  return (
+    <>
+      <div className="bg-slate-700 rounded-xl text-white">
+        <div className="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
+          <div className="text-center uppercase">Premium</div>
+          <h2 className="mt-10 font-serif text-5xl text-center">2 TB</h2>
+          <h3 className="mt-2 text-center">$8.99/Month</h3>
+          <div className="flex justify-center">
+            <a
+              href="#"
+              className="inline-block px-10 py-3 my-6 text-center border border-violet-600 rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800"
+            >
+              Purchase
+            </a>
+          </div>
+        </div>
+
+        <div className="border-t border-slate-700" />
+
+        <div className="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
+          <div className="flex flex-col space-y-2">
+            {items.map((item: any) => {
+              return (
+                <div className="flex justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M5 12l5 5l10 -10" />
+                  </svg>
+                  <span className="text-sm ml-1">{item}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Column3;
