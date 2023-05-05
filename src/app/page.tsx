327c9a4dd5f6f51798d7e48a30e785554d9ffa9f
kenvilar <kenvilar@gmail.com>
2023-05-05 11:54:42 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-slate-800">
-        <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+        <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0 test">
           <Column1 />
 
           <Column2 />
